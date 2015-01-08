2e67afdf5d094e29e96ef2f3aba0b4029a0b7e85
HEnG <heng4786@gmail.com>
2015-01-08 22:16:18 +0800

    fix search page.

--- routes/index.js
+++ routes/index.js
@@ -95,6 +95,23 @@ module.exports = function(app) {
 		});
 	});
 
+	app.get('/search', function(req, res) {
+		Post.search(req.query.keyword, function(err, posts) {
+			if (err) {
+				req.flash('error', err);
+				return res.redirect('/');
+			}
+			res.render('search', {
+				title: "SEARCH:" + req.query.keyword,
+				keyword: req.query.keyword,
+				posts: posts,
+				user: req.session.user,
+				success: req.flash('success').toString(),
+				error: req.flash('error').toString()
+			});
+		});
+	});
+
 	app.get('/post', checkLogin);
 	app.get('/post', function(req, res) {
 		res.render('post', {
