543347406680d9cb5e6cbdebb2a9aab507b7f13f
Harris <harris.heng@toursforfun.com>
2015-01-09 16:20:10 +0800

    add gulp-watch,but there are some bug need to fix.

--- gulpfile.js
+++ gulpfile.js
@@ -4,37 +4,45 @@ var gulp = require('gulp'),
     minifycss = require('gulp-minify-css'),
     jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
     cache = require('gulp-cache'),
     livereload = require('gulp-livereload'),
-    del = require('del');
+    del = require('del'),
+    watch = require('gulp-watch');
 
 // STYLES
 gulp.task('styles', function() {
-  return gulp.src('src/styles/*.less')
-    .pipe(less())
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    // .pipe(gulp.dest('public/stylesheets/'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest('public/stylesheets/'))
-    .pipe(notify({ message: 'Styles task complete' }));
+    return gulp.src('src/styles/*.less')
+        .pipe(less())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        // .pipe(gulp.dest('public/stylesheets/'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('public/stylesheets/'))
+        .pipe(notify({
+            message: 'Styles task complete'
+        }));
 });
 
 // SCRIPTS
 gulp.task('scripts', function() {
-  return gulp.src('src/scripts/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(concat('main.js'))
-    // .pipe(gulp.dest('public/scripts/'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(uglify())
-    .pipe(gulp.dest('public/scripts/'))
-    .pipe(notify({ message: 'Scripts task complete' }));
+    return gulp.src('src/scripts/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('main.js'))
+        // .pipe(gulp.dest('public/scripts/'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/scripts/'))
+        .pipe(notify({
+            message: 'Scripts task complete'
+        }));
 });
 
 // CLEAN
@@ -44,13 +52,31 @@ gulp.task('clean', function(cb) {
 
 // WATCH
 gulp.task('watch', function() {
-  // Watch .LESS files
-  gulp.watch('src/styles/*.less', ['styles']);
-  // Watch .js files
-  gulp.watch('src/scripts/**/*.js', ['scripts']);
+    // Watch .LESS files
+    gulp.watch('src/styles/*.less', ['styles']);
+    // Watch .js files
+    gulp.watch('src/scripts/**/*.js', ['scripts']);
 });
 
+var lessFiles = "src/styles/*.less";
 
-gulp.task('default', ['clean'], function() {
-    gulp.start('styles', 'scripts');
+gulp.task('default', function() {
+    watch(lessFiles, function(files) {
+        return less2css(files);
+    });
 });
+
+function less2css(files) {
+    return files
+        .pipe(less())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        // .pipe(gulp.dest('public/stylesheets/'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('public/stylesheets/'))
+        .pipe(notify({
+            message: 'Styles task complete'
+        }));
+}
